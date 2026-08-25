9dd2128b7c0d21388571b5c8e2310628210f6f51
lordFullstack <jolughi.inversiones@gmail.com>
2026-08-24 21:47:18 -0500

    Delete src directory

--- src/domain/entities/customer.entity.ts
+++
@@ -1,9 +0,0 @@
-export interface Customer {
-  id: string;
-  name: string;
-  taxId: string | null;
-  phone: string | null;
-  email: string | null;
-  address: string | null;
-  isActive: boolean;
-}
